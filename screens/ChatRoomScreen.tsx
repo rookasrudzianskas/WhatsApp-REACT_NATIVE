6589dbc7c49e5f8521e95d47fdc2f52d1f8d0118
Rokas <rookas.rudzenskas@gmail.com>
2021-07-05 11:11:24 +0300

    Roaks

--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import {View, Text, FlatList} from "react-native";
+import {View, Text, FlatList, ImageBackground} from "react-native";
 import {useRoute} from "@react-navigation/native";
 import chatRoomData from "../data/Chats";
 import ChatMessage from "../components/ChatMessage";
+// @ts-ignore
+import BG from "../assets/images/BG.png";
+
 
 const ChatRoomScreen = () => {
 
@@ -11,7 +14,12 @@ const ChatRoomScreen = () => {
 
     return (
     // @ts-ignore
+        <ImageBackground style={{ width: '100%', height: '100%' }} source={BG}>
+    {/*// @ts-ignore*/}
             <FlatList data={chatRoomData.messages} renderItem={({item}) => <ChatMessage message={item}  /> } inverted />
+
+
+        </ImageBackground>
     );
 };
 
