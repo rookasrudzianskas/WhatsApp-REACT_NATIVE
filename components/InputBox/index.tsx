bcb415b4fc7d706b60f8ffcc682e78b3d3e8d8ea
Rokas <rookas.rudzenskas@gmail.com>
2021-07-05 13:15:18 +0300

    Rokas

--- components/InputBox/index.tsx
+++ components/InputBox/index.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import styles from "./styles";
 import {View, Text, TextInput, KeyboardAvoidingView} from "react-native";
-import {MaterialCommunityIcons, FontAwesome5, Entypo, Fontisto} from "@expo/vector-icons";
+import {MaterialCommunityIcons, FontAwesome5, Entypo, Fontisto, MaterialIcons} from "@expo/vector-icons";
 
 const InputBox = () => {
 
@@ -12,7 +12,7 @@ const InputBox = () => {
         <KeyboardAvoidingView behavior="height" style={styles.container}>
             <View style={styles.mainContainer}>
                 <FontAwesome5 name="laugh-beam" size={24} color="grey" />
-                <TextInput value={message} onChangeText={(text) => setMessage(text)} style={styles.textInput} multiline/>
+                <TextInput placeholder="Type a message" value={message} onChangeText={(text) => setMessage(text)} style={styles.textInput} multiline/>
                 <Entypo name="attachment" size={24} color="grey" style={styles.icon} />
                 <Fontisto name="camera" size={24} color="grey" style={styles.icon}  />
 
@@ -20,7 +20,13 @@ const InputBox = () => {
             </View>
 
             <View style={styles.buttonContainer}>
-                <MaterialCommunityIcons name="microphone" size={28} color="white" />
+                {
+                    !message ? (
+                        <MaterialCommunityIcons name="microphone" size={28} color="white" />
+                    ) : (
+                        <MaterialIcons name="send" size={28} color={"white"} />
+                    )
+                }
             </View>
         </KeyboardAvoidingView>
     );
