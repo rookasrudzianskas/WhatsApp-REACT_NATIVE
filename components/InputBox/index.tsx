deacd79182978e2b82d06e07330416b2823f285d
Rokas <rookas.rudzenskas@gmail.com>
2021-07-05 11:18:10 +0300

    Rokas

--- components/InputBox/index.tsx
+++ components/InputBox/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import styles from "./styles";
 import {View, Text} from "react-native";
+import {MaterialCommunityIcons} from "@expo/vector-icons";
 
 const InputBox = () => {
     return (
-        <View>
-            <Text>Hello There</Text>
+        <View style={styles.container}>
+            <View style={styles.mainContainer}>
+
+            </View>
+
+            <View>
+                <MaterialCommunityIcons name="microphone" size={24} color="white" />
+            </View>
         </View>
     );
 }
