bee51ad780f0ccb8f18b5dfd82127f9baa6872f9
Rokas <rookas.rudzenskas@gmail.com>
2021-07-05 13:03:02 +0300

    Rokas

--- components/InputBox/index.tsx
+++ components/InputBox/index.tsx
@@ -8,15 +8,15 @@ const InputBox = () => {
         <View style={styles.container}>
             <View style={styles.mainContainer}>
                 <FontAwesome5 name="laugh-beam" size={24} color="grey" />
-                <TextInput/>
-                <Entypo name="attachment" size={24} color="grey" />
-                <Fontisto name="camera" size={24} color="grey" />
+                <TextInput style={styles.textInput}/>
+                <Entypo name="attachment" size={24} color="grey" style={styles.icon} />
+                <Fontisto name="camera" size={24} color="grey" style={styles.icon}  />
 
 
             </View>
 
             <View style={styles.buttonContainer}>
-                <MaterialCommunityIcons name="microphone" size={24} color="white" />
+                <MaterialCommunityIcons name="microphone" size={28} color="white" />
             </View>
         </View>
     );
--- components/InputBox/styles.ts
+++ components/InputBox/styles.ts
@@ -1,7 +1,35 @@
 import {StyleSheet} from "react-native";
+import Colors from "../../constants/Colors";
 
 const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        margin: 10,
 
+    },
+    mainContainer: {
+        flexDirection: "row",
+        backgroundColor: "white",
+        padding: 10,
+        borderRadius: 50,
+        marginRight: 10,
+        flex: 1,
+    },
+    textInput: {
+        flex: 1,
+        marginHorizontal: 10,
+    },
+    icon: {
+      marginHorizontal: 5,
+    },
+    buttonContainer: {
+        backgroundColor: Colors.light.tint,
+        borderRadius: 50,
+        width: 50,
+        height: 50,
+        justifyContent: "center",
+        alignItems: "center",
+    }
 });
 
 export default styles;
