b45b684ccdc2f3b6c99daed21f37ce6cc5012a25
Rokas <rookas.rudzenskas@gmail.com>
2021-07-05 12:56:44 +0300

    Rokas

--- components/InputBox/index.tsx
+++ components/InputBox/index.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import styles from "./styles";
-import {View, Text} from "react-native";
-import {MaterialCommunityIcons} from "@expo/vector-icons";
+import {View, Text, TextInput} from "react-native";
+import {MaterialCommunityIcons, FontAwesome5, Entypo, Fontisto} from "@expo/vector-icons";
 
 const InputBox = () => {
     return (
         <View style={styles.container}>
             <View style={styles.mainContainer}>
+                <FontAwesome5 name="laugh-beam" size={24} color="grey" />
+                <TextInput/>
+                <Entypo name="attachment" size={24} color="grey" />
+                <Fontisto name="camera" size={24} color="grey" />
+
 
             </View>
 
-            <View>
+            <View style={styles.buttonContainer}>
                 <MaterialCommunityIcons name="microphone" size={24} color="white" />
             </View>
         </View>
