7c420a743585fc5aaa754bfb84b85478ee822078
Rokas <rookas.rudzenskas@gmail.com>
2021-07-06 08:34:28 +0300

    Rokas

--- components/NewMessageButton/index.tsx
+++ components/NewMessageButton/index.tsx
@@ -17,7 +17,6 @@ const NewMessageButton = () => {
             <View>
                 <MaterialCommunityIcons name="message-reply-text" size={28} color="white" />
                 <MaterialCommunityIcons name="message-reply-text" size={28} color="white" />
-                <MaterialCommunityIcons name="message-reply-text" size={28} color="white" />
             </View>
         </TouchableOpacity>
     )
