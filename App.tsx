d45028246102bae630f5186c97566aa2d2fbcdeb
Rokas <rookas.rudzenskas@gmail.com>
2021-07-21 12:24:14 +0300

    Rokas

--- App.tsx
+++ App.tsx
@@ -6,13 +6,14 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-import Amplify from 'aws-amplify';
+import Amplify, {Auth, API, graphqlOperation} from 'aws-amplify';
 import config from './aws-exports';
 
 Amplify.configure(config);
 
 // @ts-ignore
 import { withAuthenticator, AmplifySignOut } from 'aws-amplify-react-native';
+import {getUser} from "./src/graphql/queries";
 
 
 function App() {
@@ -21,6 +22,26 @@ function App() {
 
   useEffect(() => {
     // run this code then the application loads
+    const fetchUser = async () => {
+      // get authenticated user from auth
+
+      const userInfo = await Auth.currentAuthenticatedUser({bypassCache: true});
+      console.log(userInfo);
+
+      if(userInfo) {
+      // get the user from backend with the user ID from AUth
+        const userData = await API.graphql(graphqlOperation(getUser, {id: userInfo.attributes.sub}));
+        console.log(userData);
+
+
+
+      // if there is no user in DB with the id then create one
+
+      }
+
+    }
+
+    fetchUser();
 
   }, []);
 
